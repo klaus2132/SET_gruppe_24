b8d24f03512556beaac023aa03e3d2c40568b00e
klaus2132 <klaushaugness@gmail.com>
2024-10-30 11:51:32 +0100

    la til 2 klasser
    La til securityCamera og SmartPlug

--- src/models/SecurityCamera.java
+++ src/models/SecurityCamera.java
@@ -1,4 +1,25 @@
 package models;
 
-public class SecurityCamera {
+public class SecurityCamera  extends Unit{
+    private boolean recording;
+    public SecurityCamera(int id, String name) {
+        super(id, name);
+        this.recording = false;
+    }
+
+    //gettere og settere
+    public boolean getRecording() {
+        return recording;
+    }
+    public void setRecording(boolean recording) {
+        this.recording = recording;
+    }
+
+    //metoder for start og stop av filming
+    public void startRecording(){
+        this.recording = true;
+    }
+    public void stopRecording(){
+        this.recording = false;
+    }
 }
--- src/models/SmartPlug.java
+++ src/models/SmartPlug.java
@@ -1,4 +1,29 @@
 package models;
 
-public class SmartPlug {
+public class SmartPlug extends Unit{
+
+    private boolean power;
+    private int wattage;
+    public SmartPlug(int id, String name){
+        super(id, name);
+        this.power = false;
+        this.wattage = wattage;
+    }
+
+    //gettere og settere
+
+    public int getWattage() {
+        return wattage;
+    }
+    public void setWattage(int wattage) {
+        this.wattage = wattage;
+    }
+
+    public void setPower(boolean power) {
+        this.power = power;
+    }
+
+    public boolean getPower() {
+        return power;
+    }
 }
