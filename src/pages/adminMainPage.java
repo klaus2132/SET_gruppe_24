fd3054cf219a145ed76b474632768f3167dfb661
klaus2132 <klaushaugness@gmail.com>
2024-11-19 16:25:00 +0100

    dette er en fungerende versjon!
    Fikset admin

--- src/pages/adminMainPage.java
+++ src/pages/adminMainPage.java
@@ -1,14 +1,15 @@
 package pages;
 
-import models.Unit;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class adminMainPage extends JDialog{
     private JPanel adminPanel;
     private JLabel adminHeading;
-    private JTable unitTable;
+    private JButton btnShortcuts;
+    private JButton btnUnits;
 
     public adminMainPage(JFrame parent){
         super(parent);
@@ -18,8 +19,27 @@ public class adminMainPage extends JDialog{
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        btnUnits.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Åpner Enheter");
+            }
+        });
+        btnShortcuts.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Åpner Snarveier");
+            }
+        });
+
 
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Hello world");
+
+        adminMainPage frame = new adminMainPage(null);
     }
 
 }
